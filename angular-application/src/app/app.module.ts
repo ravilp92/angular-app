f6ea02f2f7e46fc02361f4a9cbe9c7a14caa5d3b
Ravi Patel <40341676+ravilp92@users.noreply.github.com>
2020-09-06 19:08:18 -0400

    Added Menu Flipper

--- angular-application/src/app/app.module.ts
+++ angular-application/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
@@ -18,7 +18,7 @@ import { ContactComponent } from './components/contact/contact.component';
     ProjectsComponent,
     ContactComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
